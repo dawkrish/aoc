6ed8b6cca557fda9f600f777054e6767fe62e635
dawkrish <krishnansh710@gmail.com>
2025-06-22 00:05:25 +0530

    [ADD] 2015 folder day1

---
+++ 2015/src/Day1.hs
@@ -0,0 +1,22 @@
+module Day1 where
+
+main :: IO ()
+main = do
+  file <- readFile "inputs/Day1.txt"
+  print ("file length is" ++ show (length file))
+  let ans1 = part1 file
+  print ("Answer of par1 is " ++ show ans1)
+  let ans2 = part2 file
+  print ("Answer of par2 is " ++ show ans2)
+
+
+count :: Eq a => a -> [a] -> Int
+count c = length . filter (== c)
+
+part1 :: String -> Int
+part1 = foldl (\x y -> if y == '(' then x + 1 else x - 1) 0 
+ 
+
+part2 :: String  -> Int
+part2 = length . takeWhile (>= 0) . scanl (\x y -> if y == '(' then x + 1 else x - 1) 0
+
