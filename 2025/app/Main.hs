adf1c526de89d2749021d68df8c46da85f819c0f
dawkrish <krishnansh710@gmail.com>
2025-12-06 11:54:18 +0530

    init file structure

--- 2025/app/Main.hs
+++ 2025/app/Main.hs
@@ -1,10 +1,43 @@
 module Main where
 
-import qualified Day1 as Day1
+import qualified Day1
+import qualified Day10
+import qualified Day11
+import qualified Day12
+import qualified Day2
+import qualified Day3
+import qualified Day4
+import qualified Day5
+import qualified Day6
+import qualified Day7
+import qualified Day8
+import qualified Day9
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
+  args <- getArgs
+  let day = getDay args
+  runDay day
   putStrLn "Hello, Haskell!"
 
-runDay :: Int -> IO ()
-runDay 1 = Day1.run
+getDay :: [String] -> Maybe Int
+getDay [] = Nothing
+getDay (x : _) = read x
+
+runDay :: Maybe Int -> IO ()
+runDay Nothing = putStrLn "unable to parse day"
+runDay (Just x) = case x of
+  1 -> Day1.run
+  2 -> Day2.run
+  3 -> Day3.run
+  4 -> Day4.run
+  5 -> Day5.run
+  6 -> Day6.run
+  7 -> Day7.run
+  8 -> Day8.run
+  9 -> Day9.run
+  10 -> Day10.run
+  11 -> Day11.run
+  12 -> Day12.run
+  _ -> putStrLn "Invalid day"
---
+++ 2025/src/Day10.hs
@@ -0,0 +1,16 @@
+module Day10 where
+
+run :: IO ()
+run = do
+  file <- readFile "inputs/01.txt"
+  let p1 = part1 file
+  let p2 = part2 file
+  putStrLn $ "Answer for part1:" ++ show p1
+  putStrLn $ "Answer for part2:" ++ show p2
+  return ()
+
+part1 :: String -> Int
+part1 file = 0
+
+part2 :: String -> Int
+part2 file = 0
