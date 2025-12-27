da16b332d84d20709c8f3d0c23858b71bcf6d0a2
dawkrish <krishnansh710@gmail.com>
2025-12-27 22:14:09 +0530

    part1:day3 done

--- 2025/src/Day3.hs
+++ 2025/src/Day3.hs
@@ -2,15 +2,25 @@ module Day3 where
 
 run :: IO ()
 run = do
-  file <- readFile "inputs/01.txt"
+  file <- readFile "inputs/03.txt"
   let p1 = part1 file
   let p2 = part2 file
-  putStrLn $ "Answer for part1:" ++ show p1
-  putStrLn $ "Answer for part2:" ++ show p2
+  putStrLn $ "Answer for part1: " ++ show p1
+  putStrLn $ "Answer for part2: " ++ show p2
   return ()
 
 part1 :: String -> Int
-part1 file = 0
+part1 file = sum $ map (process . parse) (lines file)
 
 part2 :: String -> Int
 part2 file = 0
+
+parse :: String -> [Int]
+parse = map (\x -> read [x] :: Int)
+
+process :: [Int] -> Int
+process xs = maximum $ go xs []
+  where
+    go [] acc = acc
+    go [_] acc = acc
+    go (y : ys) acc = go ys ((10 * y) + maximum ys : acc)
